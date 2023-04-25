45e432afedfcf9b984b4779ca22969aad236915a
vitorokubo <vitor.okubo@hotmail.com>
2023-04-25 09:16:03 -0300

    refactor: correção do pwa

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue(), VitePWA({ registerType: 'autoUpdate', scope: '/' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
